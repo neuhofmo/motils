bfdf3d5e4086958aaae7ce3780fdb2480411dec7
neuhofmo <neuhofmo@gmail.com>
2017-02-08 13:51:58 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='motils',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Moran Neuhof',
+    author_email='neuhofmo@gmail.com',
+    description='A package containing local utilities for simplifying every day coding and scripting',
+    long_description='A package containing local utilities for simplifying every day coding and scripting. '
+                     'These include: performing zip on a list of files, performing tar+gz on a folder,'
+                     'Converting files to strings, checking if files exist and not empty, etc.'
+
+)
